841b6d532a022b50499a8b115c16db6a9282809d
Colin Williams <williamsca20@gmail.com>
2021-06-05 12:28:59 -0400

    🌐 Turned site into a Progressive Web App!

---
+++ serviceWorker.js
@@ -0,0 +1,38 @@
+// On install - the application shell cached
+self.addEventListener('install', (e) => {
+	e.waitUntil(
+		caches.open('sw-cache').then((cache) => {
+			// HTML
+			cache.add('/index.html')
+			cache.add('/feedback.html')
+			// CSS
+			cache.add('/assets/css/style.css')
+			// JS
+			cache.add('/assets/js/formReset.js')
+			cache.add('/assets/js/index.js')
+			cache.add('/assets/js/Metronome.js')
+			cache.add('/assets/js/Move.js')
+			cache.add('/assets/js/Turn.js')
+			cache.add('/assets/js/unmute.js')
+			cache.add('/assets/images/marching-shoe.svg')
+			// images
+			cache.add('/assets/images/metronome.svg')
+			// external resources
+			cache.add('https://use.fontawesome.com/releases/v5.15.3/css/all.css')
+			cache.add(
+				'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap'
+			)
+		})
+	)
+})
+
+// with requeset network
+self.addEventListener('fetch', (e) => {
+	e.respondWith(
+		// try the cache
+		caches.match(e.request).then((response) => {
+			// return it if there is a response, or else fetch again
+			return response || fetch(e.request)
+		})
+	)
+})
